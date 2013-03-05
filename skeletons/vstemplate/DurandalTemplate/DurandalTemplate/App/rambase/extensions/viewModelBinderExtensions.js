2aec64d2b9c76d584df189927242918e64ee7988
Hasith Yaggahavita <hasith@gmail.com>
2013-03-05 14:43:21 +0530

    viewModelBinder extended to override beforeBind method

--- skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/rambase/extensions/viewModelBinderExtensions.js
+++ skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/rambase/extensions/viewModelBinderExtensions.js
@@ -1,67 +1,20 @@
 ﻿define(function (require) {
-    var system = require('durandal/system');
     var viewModelBinder = require('durandal/viewModelBinder');
 
-    var insufficientInfoMessage = 'Insufficient Information to Bind';
-    var unexpectedViewMessage = 'Unexpected View Type';
+    var beforeBind = function (obj, view) {
+        var bindableElements = $(view).find('[data-bind]');
 
-    function doBind(obj, view, action) {
-        if (!view || !obj) {
-            if (viewModelBinder.throwOnErrors) {
-                throw new Error(insufficientInfoMessage);
-            } else {
-                system.log(insufficientInfoMessage, view, obj);
-            }
-            return;
+        var kendoElements = $(view).find('[data-mvvm-framework=kendo]');
+        $(kendoElements).wrapAll('<div data-bind="stopBinding: true"></div>');
+        for (var i = 0; i < kendoElements.length; i++) {
+            kendo.bind(kendoElements[i], obj.viewModel);
         }
-
-        if (!view.getAttribute) {
-            if (viewModelBinder.throwOnErrors) {
-                throw new Error(unexpectedViewMessage);
-            } else {
-                system.log(unexpectedViewMessage, view, obj);
-            }
-            return;
-        }
-
-        var viewName = view.getAttribute('data-view');
-
-        try {
-            system.log('Binding', viewName, obj);
-
-            viewModelBinder.beforeBind(obj, view);
-            action();
-            viewModelBinder.afterBind(obj, view);
-        } catch (e) {
-            if (viewModelBinder.throwOnErrors) {
-                throw new Error(e.message + ';\nView: ' + viewName + ";\nModuleId: " + system.getModuleId(obj));
-            } else {
-                system.log(e.message, viewName, obj);
-            }
-        }
-    }
-
-    var bind = function (obj, view) {
-        doBind(obj, view, function () {
-            var bindableElements = $(view).find('[data-bind]');
-
-            var kendoElements = $(view).find('[data-mvvm-framework=kendo]');
-            $(kendoElements).wrapAll('<div data-bind="stopBinding: true"></div>');
-            for (var i = 0; i < kendoElements.length; i++) {
-                kendo.bind(kendoElements[i], obj.viewModel);
-            }
-
-            ko.applyBindings(obj, view);
-            if (obj.setView) {
-                obj.setView(view);
-            }
-        });
     }
 
 
     return {
         extend: function () {
-            viewModelBinder.bind = bind;
+            viewModelBinder.beforeBind = beforeBind;
         }
 
     }
--- skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/rambase/rb.js
+++ skeletons/vstemplate/DurandalTemplate/DurandalTemplate/App/rambase/rb.js
@@ -4,8 +4,7 @@
 
         applyExtensions: function () {
             require('rambase/extensions/routerExtensions').extend();
-            //require('rambase/extensions/viewModelBinderExtensions').extend();
-            require('rambase/extensions/applyBindingsExtensions').extend();
+            require('rambase/extensions/viewModelBinderExtensions').extend();
             require('rambase/extensions/knockout-extensions').extend();
         }
 
